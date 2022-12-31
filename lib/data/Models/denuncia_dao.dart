c52103c3f87d34759a723dbca6429e214597583f
Mr-Kind16 <arturogentile36@gmail.com>
2022-12-31 15:08:58 +0100

    Commit per mettermi in pari

--- lib/data/Models/denuncia_dao.dart
+++ lib/data/Models/denuncia_dao.dart
@@ -1,27 +1,21 @@
-import 'dart:developer';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+import '../../domain/entity/denuncia_entity.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:report_it/domain/entity/denuncia_entity.dart';
 
-var db = FirebaseFirestore.instance;
 
-class DenunciaDao {
-  static Future<DocumentReference<Object?>> addDenuncia(Denuncia denuncia) {
-    Future<DocumentReference<Object?>> id = db
-        .collection("Denuncia")
-        .add(denuncia.toMap())
-      ..then((doc) => log('Data added with success with ID: ${doc.id}'));
-    return id;
-  }
+FirebaseFirestore db= FirebaseFirestore.instance;
 
-  static void updateId(String id) async {
-    DocumentReference? returnCode;
-    try {
-      returnCode = FirebaseFirestore.instance.collection('Denuncia').doc(id);
-      await returnCode.update({"ID": id});
-    } catch (e) {
-      log("Error: ");
-      return null;
-    }
-  }
+Future<Denuncia?> RetriveDenunciaById(String id) async{
+  var ref= db.collection("Denuncia").doc(id);
+
+  await ref.get().then((value){
+    Denuncia d= Denuncia.fromJson(value.data()!);
+    return d;
+  });
 }
+
+
+
