ec1ddb7f2c28a392e7306e06d50aec54b46db787
Mr-Kind16 <arturogentile36@gmail.com>
2023-01-04 14:17:54 +0100

    fix denuncia controller

--- lib/domain/repository/denuncia_controller.dart
+++ lib/domain/repository/denuncia_controller.dart
@@ -13,17 +13,12 @@ class DenunciaController {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<List<Denuncia>> visualizzaDenunceByUtente() async {
-    try {
       final User? user = auth.currentUser;
       if (user == null) {
         print("NON SEI LOGGATO biscottooo");
       } else {
         return await denunciaDao.retrieveByUtente(user.uid);
       }
-    } catch (e) {
-      print(e);
-      rethrow;
-    }
     return Future.error(StackTrace);
   }
 
