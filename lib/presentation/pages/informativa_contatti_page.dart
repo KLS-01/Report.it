0c88e585a303175e31e234edfd215a2d497f56df
KLS-01 <lnrdschv@gmail.com>
2023-01-04 02:12:10 +0100

    Carousel for "Informativa e Contatti" + GUI change

--- lib/presentation/pages/informativa_contatti_page.dart
+++ lib/presentation/pages/informativa_contatti_page.dart
@@ -1,42 +1,88 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Informativa extends StatelessWidget {
+class Informativa extends StatefulWidget {
+  @override
+  State<Informativa> createState() => _InformativaState();
+}
+
+class _InformativaState extends State<Informativa> {
+  int _current = 0;
+
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       color: Color.fromRGBO(255, 254, 248, 1),
       duration: const Duration(seconds: 1),
-      child: PageView(
-        // onPageChanged: _onPageChanged,
-        children: <Widget>[
-          ListView(
-            children: [
-              PagerPageWidget_noimage(
-                text: 'Informativa....',
-                description: 'Hai bisogno di aiuto? Ecco qui una sezione FAQ!',
-                //image: Image.asset('images/C11_Logo-png.png'), questo non si vede perché ho cambiato widget
-              ),
-              Card1(
-                titolo: legge,
-                corpo: loremIpsum,
-              ),
-              Card1(
-                titolo: denuncia,
-                corpo: loremIpsum,
-              ),
-              Card1(
-                titolo: psicologo,
-                corpo: loremIpsum,
-              ),
-              Card1(
-                titolo: mappa,
-                corpo: loremIpsum,
-              ),
-            ],
+      child: ListView(
+        children: [
+          PagerPageWidget_noimage(
+            text: 'Informativa....',
+            description: 'Hai bisogno di aiuto? Ecco qui una sezione FAQ!',
+            //image: Image.asset('images/C11_Logo-png.png'), questo non si vede perché ho cambiato widget
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              enableInfiniteScroll: false,
+              viewportFraction: 1,
+              height: MediaQuery.of(context).size.height * 0.35,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              },
+            ),
+            items: carousels.map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Color.fromARGB(255, 246, 208, 97)),
+                      child: Column(
+                        children: [
+                          Text(
+                            '${i.title}',
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          Image.asset(
+                            i.imagePath,
+                            scale: 7,
+                          ),
+                          Text(
+                            '${i.title}',
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ],
+                      ));
+                },
+              );
+            }).toList(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: carousels.asMap().entries.map((entry) {
+              return GestureDetector(
+                // onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: (Theme.of(context).brightness == Brightness.dark
+                              ? Colors.white
+                              : Color.fromRGBO(219, 29, 69, 1))
+                          .withOpacity(_current == entry.key ? 1.0 : 0.4)),
+                ),
+              );
+            }).toList(),
           ),
           PagerPageWidget(
             text: '....e Contatti!',
@@ -50,13 +96,47 @@ class Informativa extends StatelessWidget {
 
   final loremIpsum =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
   final legge = "Quali sono le leggi anti-discriminazione?";
+
   final denuncia = 'Come posso inoltrare una denuncia?';
+
   final psicologo = 'Come posso prenotare una visita psicologica?';
+
   final mappa = 'Come posso trovare la caserma o ospedale più vicino a me?';
+
+  List<ContentsInformation> carousels = [
+    ContentsInformation(
+        title: 'title1',
+        body: 'testo1',
+        imagePath: 'assets/images/flutter1.png'),
+    ContentsInformation(
+        title: 'title2',
+        body: 'testo2',
+        imagePath: 'assets/images/flutter1.png'),
+    ContentsInformation(
+        title: 'title3',
+        body: 'testo3',
+        imagePath: 'assets/images/flutter1.png'),
+    ContentsInformation(
+        title: 'title4',
+        body: 'testo4',
+        imagePath: 'assets/images/flutter1.png'),
+    ContentsInformation(
+        title: 'title5',
+        body: 'testo5',
+        imagePath: 'assets/images/flutter1.png'),
+  ];
 }
 
 // WIDGET INFORMATIVA E CONTATTI CON IMMAGINE
+class ContentsInformation {
+  String title, body, imagePath;
+
+  ContentsInformation(
+      {required this.title, required this.body, required this.imagePath});
+}
+
 class PagerPageWidget extends StatelessWidget {
   final String? text;
   final String? description;
