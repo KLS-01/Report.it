fdb426b0b736fddcb26badc6f84f902779fe28fb
Mr-Kind16 <arturogentile36@gmail.com>
2023-01-06 13:23:34 +0100

    quick fix ultimo commit

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -6,7 +6,7 @@ import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
 import 'package:report_it/presentation/pages/Informativa_contatti.dart';
 import 'package:report_it/presentation/pages/fake_index.dart';
-import 'package:report_it/presentation/pages/test_provider.dart';
+import 'package:report_it/presentation/pages/fake_index.dart';
 import 'package:report_it/presentation/pages/visualizza_storico_denunce_page.dart';
 import '../../domain/repository/authentication_service.dart';
 import 'informativa_contatti_page.dart';
