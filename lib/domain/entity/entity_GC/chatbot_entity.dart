ea4a47d9f47d0186a02313f9c62525ac329c6037
KLS-01 <lnrdschv@gmail.com>
2023-01-20 00:46:43 +0100

    Adapter chatbot

--- lib/domain/entity/entity_GC/chatbot_entity.dart
+++ lib/domain/entity/entity_GC/chatbot_entity.dart
@@ -5,17 +5,6 @@ class ChatB {
 
   ChatB(this.domanda, this.risposta, {this.id});
 
-  factory ChatB.fromJson(Map<String, dynamic> json) {
-    var c = ChatB(json["Domanda"], json["Risposta"]);
-    return c;
-  }
-
-  factory ChatB.fromMap(map) {
-    var c = ChatB(map["Creatore"], map["Risposta"]);
-
-    return c;
-  }
-
   Map<String, dynamic> toMap() {
     return {
       "Domanda": domanda,
